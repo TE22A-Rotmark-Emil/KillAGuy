583d3185a9688c94b3d25708fe33afdea0a06c1a
Emil Rotmark <emil.rotmark@elev.ga.ntig.se>
2024-09-05 15:25:52 +0200

    initialised repos

---
+++ KillAGuy/NPC.cs
@@ -0,0 +1,6 @@
+public class NPC(){
+    public string name {get; set;}
+    public int HP {get; set;}
+    public int Strength {get; set;}
+    public float accuracy {get; set;}
+}
---
+++ KillAGuy/Program.cs
@@ -0,0 +1,13 @@
+﻿static void Main(){
+    NPC alt = new(){
+        name = "Henry",
+        HP = 20,
+        Strength = 3,
+        accuracy = 100
+    };
+
+    Console.WriteLine(alt.Strength);
+    Console.ReadLine();
+};
+
+Main();
